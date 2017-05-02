f63b5ba7d9ae0687406656366e2d032ceb47d32e
Alex Horton <alexhortonmusic@gmail.com>
2017-05-02 09:49:42 -0500

    basic proof of concept

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This is a simple test app to learn how to use the Wordpress API
+## This is a simple test app to learn how to use the Wordpress API
--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $('#new_url').submit(function(e) {
     rest_url = clean_url + '/wp-json/wp/v2/'
 
     // get posts from new URL
-    console.log(rest_url)
+    get_post_list()
   }
 
   catch (e) {
@@ -24,3 +24,111 @@ $('#new_url').submit(function(e) {
     console.log('Error: User input is incorrect.')
   }
 })
+
+function get_post_list() {
+
+  let json_url = rest_url + 'posts/'
+
+  $.ajax({
+    dataType: 'json',
+    url: json_url
+  })
+  .done(function(obj) {
+    create_post_list(obj)
+  })
+  .fail(function() {
+    $('.site-header').append(`<div class="error">That didn&rsquo;t work. Try a different url.</div>`)
+    console.log('Error: AJAX returned nothing.')
+  })
+}
+
+function create_post_list(obj) {
+  $('.post-list').empty().append(`<ul></ul>`)
+  let navListItem
+
+  for ( let i = 0; i < obj.length; i++ ) {
+    navListItem =
+    `
+      <li class='post-links'>
+        <a href='javascript:void(0)' data-id='${obj[i].id}'>
+          ${obj[i].title.rendered}
+        </a>
+      </li>
+    `
+    $('.post-list ul').append(navListItem)
+  }
+
+  post_trigger()
+}
+
+function post_trigger() {
+  // grabs clicked post id and gets object
+  $('.post-links a').on('click', get_post)
+  // auto "clicks" on most recent post
+  $('.post-links a').first().trigger('click')
+}
+
+function get_post() {
+  let post_id = $(this).attr('data-id')
+  console.log(post_id)
+
+  // create request
+  let json_url = rest_url + 'posts/' + post_id + '?_embed=true'
+
+  $.ajax({
+    dataType: 'json',
+    url: json_url
+  })
+  .done(function(obj) {
+    // get post data
+    the_post_data(obj)
+  })
+  .fail(function() {
+    $('.site-header').append(`<div class="error">That didn&rsquo;t work. Try a different url.</div>`)
+    console.log('Error: Single post error.')
+  })
+}
+
+function the_post_data(obj) {
+  // get featured img
+  let featured_img_ID = obj.featured_media
+
+  let feat_img
+
+  build_post()
+
+  // create html for post
+  function output_the_img() {
+    let img = obj._embedded['wp:featuredmedia'][0]
+    let img_large = ''
+    let img_width = img.media_details.sizes.full.width
+    let img_height = img.media_details.sizes.full.height
+    if (img.media_details.sizes.hasOwnProperty('large')) {
+      img_large = img.media_details.sizes.large.source_url + '1024w'
+    }
+    feat_img =
+    `
+      <div class='featured-img'>
+        <img src='${img.media_details.sizes.full.source_url}' width='${img_width}' height='${img_height}' class='the-featured-img' alt='' sizes='100vw' />
+      </div>
+    `
+    return feat_img
+  }
+
+  function build_post() {
+    let date = new Date(obj.date)
+    if (featured_img_ID !== 0) {
+      if ($('.featured-img').length) {
+        $('.featured-img').replaceWith(output_the_img())
+      } else {
+        $('.post').before(output_the_img())
+      }
+    } else {
+      $('.featured-img').remove()
+    }
+    $('.post-title').text(obj.title.rendered)
+    $('.post-author').text(obj._embedded.author[0].name)
+    $('.post-date').text(date.toDateString())
+    $('.post-content').replaceWith(`<div class='post-content'>${obj.content.rendered}</div>`)
+  }
+}
